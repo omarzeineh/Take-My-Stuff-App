268ec84cc412acbcd0dc732878d6fb52c2a90e8c
Omar Zeineh <165347268+omarzeineh@users.noreply.github.com>
2025-09-03 00:08:43 +0400

    Delete TMS/capacitor.config.ts

--- TMS/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'TMS',
-  webDir: 'www'
-};
-
-export default config;
